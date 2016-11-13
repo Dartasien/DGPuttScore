9be8536bcb3367b514e6f34886da2a5fa07e7500
Feildon Hampton <hamptfe@hotmail.com>
2016-11-13 15:02:53 -0500

    Added winner calculation and starting new game.

--- DiscGolfPuttMineGame.Portable/Views/GameRoundsX.xaml.cs
+++ DiscGolfPuttMineGame.Portable/Views/GameRoundsX.xaml.cs
@@ -1,16 +1,69 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DiscGolfPuttMiniGame.Portable.Data;
 using DiscGolfPuttMiniGame.Portable.Models;
-using Xamarin.Forms;
 
 namespace DiscGolfPuttMiniGame.Portable.Views
 {
-    public partial class GameRoundsX : ContentPage
+    public partial class GameRoundsX
     {
         private Round Round => BindingContext as Round;
 
         public GameRoundsX()
         {
             InitializeComponent();
+        }
+
+        private void FinishTurnButton_OnClicked(object sender, EventArgs e)
+        {
+            Round.IsRoundOver = true;
+            Round.IsCurrent = false;
+            var database = new ScoreTrackerDatabase();
+            database.Save(Round);
+            foreach (var turn in Round.Turns)
+            {
+                database.Save(turn);
+            }
+            var game = database.GetItem<Game>(Round.GameId);
+            if (!game.Rounds.All(r => r.IsRoundOver))
+            {
+                var roundCount = Round.RoundCount + 1;
+                var nextRound = game.Rounds.FirstOrDefault(r => r.RoundCount == roundCount);
+                nextRound.IsCurrent = true;
+                var nextRoundX = new GameRoundsX {BindingContext = nextRound};
+                Navigation.PushModalAsync(nextRoundX);
+            }
+            else
+            {
+
+                var turns = game.Rounds.SelectMany(r => r.Turns).ToList();
+                var playerScores = new Dictionary<int, int>();
+
+                foreach (var player in game.Players)
+                {
+                    if (!playerScores.ContainsKey(player.Id))
+                    {
+                        playerScores.Add(player.Id, 0);
+                    }
+                    
+                }
+
+                foreach (var turn in turns)
+                {
+                    playerScores[turn.PlayerId] += turn.Score;
+                }
+
+                foreach (var player in game.Players)
+                {
+                    if (playerScores[player.Id] <= game.WinningScore) continue;
+                    game.WinningPlayer = player;
+                    game.WinningScore = playerScores[player.Id];
+                }
+
+                var winnerNewGame = new WinnerNewGame {BindingContext = game};
+                Navigation.PushModalAsync(winnerNewGame);
+            }
         }
     }
 }
---
+++ DiscGolfPuttMineGame.Portable/Views/WinnerNewGame.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DiscGolfPuttMiniGame.Portable.Views
+{
+    public partial class WinnerNewGame
+    {
+        public WinnerNewGame()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_OnClicked(object sender, EventArgs e)
+        {
+            var mainPageX = new MainPageX();
+            Navigation.PushModalAsync(mainPageX);
+        }
+    }
+}
